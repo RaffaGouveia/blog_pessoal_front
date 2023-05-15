476a49646e5acea7ee43298353a7183c064d772c
Rafael Gouveia <raffagouveiabarros@gmail.com>
2023-05-15 14:10:40 -0300

    Criação dos componentes Navbar e Footer e refatoração do compponente Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Home from "./paginas/home/Home"
+import { useState } from 'react';
+import './App.css';
+import Navbar from "./componentes/estaticos/navbar/Navbar";
+import Home from "./paginas/home/Home";
+import Footer from "./componentes/estaticos/footer/Footer";
 
 function App() {
   return (
-    <Home/>
+    <>
+    <Navbar />
+    <Home />
+    <Footer />
+    
+    </>
   )
 }
 
