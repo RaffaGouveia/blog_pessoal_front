37f0b949d9f3a24b0b19948c81513c86e64bd9aa
Rafael Gouveia <raffagouveiabarros@gmail.com>
2023-05-18 20:25:26 -0300

    Criação do model User e adicionando regras de contrato do mesmo na service

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -8,3 +8,8 @@ export const login = async(url: any,dados: any,setDados: any) => {
     const resposta = await api.post(url,dados)
     setDados(resposta.data)
 }
+
+export const cadastroUsuario = async (url: any, dados: any, setDados: any) => {
+    const resposta = await api.post(url, dados);
+    setDados(resposta.data.token);
+  };
