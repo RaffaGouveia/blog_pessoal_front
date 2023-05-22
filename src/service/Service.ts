851b955030ddd5038e483a92e65dd0e6b12c57f7
Rafael Gouveia <raffagouveiabarros@gmail.com>
2023-05-22 08:34:32 -0300

    Alteração na service.ts

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -4,20 +4,13 @@ export const api = axios.create({
   baseURL: "https://blogpessoal-tox5.onrender.com",
 });
 
-export const login = async (url: any, dados: any, setDados: any) => {
-  const resposta = await api.post(url, dados);
-  setDados(resposta.data);
-};
-
-
-
 export const login = async(url: any,dados: any,setDados: any) => {
     const resposta = await api.post(url,dados)
-    setDados(resposta.data)
+    setDados(resposta.data.token)
 }
 
 export const cadastroUsuario = async (url: any, dados: any, setDados: any) => {
     const resposta = await api.post(url, dados);
-    setDados(resposta.data.token);
+    setDados(resposta.data);
   };
 
