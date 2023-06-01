8b2e987efc647bbf0ff1ef70772680b19fc119bd
Rafael Gouveia <raffagouveiabarros@gmail.com>
2023-06-01 10:20:36 -0300

    alterações necessarias pro deploy

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://blogpessoal-tox5.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export const login = async(url: any,dados: any,setDados: any) => {
